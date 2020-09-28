29ba6acaa8d0772045bc9b396da40d92440f4b3a
facetable-lab <diofff6@gmail.com>
2020-09-28 05:44:15 +0300

    Add caesarChiper.py

--- algorithms/caesarChiper.py
+++ algorithms/caesarChiper.py
@@ -1,1 +1,39 @@
-print('test')
+alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+
+#This function encode the message
+def encodeMessage(text, key):
+    encodedMsg = ''
+    for letter in text:
+        if alphabet.find(letter) != -1:
+            let = alphabet.find(letter)
+            encodedMsg = encodedMsg + alphabet[let + key]
+        else:
+            encodedMsg += letter
+    return encodedMsg
+
+#This function decode the message
+def decodeMessage(encMsg, key):
+    decodedMsg = ''
+    for letter in encMsg:
+        if alphabet.find(letter) != -1:
+            let = alphabet.find(letter)
+            decodedMsg = decodedMsg + alphabet[let - key]
+        else:
+            decodedMsg += letter
+    return decodedMsg
+
+
+answer = int(input('Code/Decode (1 or 0): '))
+
+if answer == 1:
+    text = input('Input text for code: ')
+    key = int(input('Enter key(2-26): '))
+    msg = encodeMessage(text, key)
+    print('Your coded message:', msg)
+elif answer == 0:
+    text = input('Input text for decode: ')
+    key = int(input('Enter key(2-26): '))
+    msg = decodeMessage(text, key)
+    print('Your decoded message:', msg)
+else:
+    print('Incorrect answer')
