e8575135016cda18559677209bdafe42b4de1456
facetable-lab <67112192+facetable-lab@users.noreply.github.com>
2020-09-30 05:33:09 +0300

    Delete old site

--- site/js/changeTheme.js
+++
@@ -1,7 +0,0 @@
-let button = document.querySelector('.btn-change-theme');
-let page = document.querySelector('.page');
-
-button.onclick = function() {
-  page.classList.toggle('light-theme');
-  page.classList.toggle('dark-theme');
-};
